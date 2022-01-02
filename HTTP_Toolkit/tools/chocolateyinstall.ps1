d0562ea7a06a887ad0a26a4ec3d34dc53aa17975
Fabian <fabian99.werner@gmail.com>
2022-01-03 00:28:30 +0100

    HTTP TOOLKIT

---
+++ HTTP_Toolkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v1.5.1/HttpToolkit-installer-1.5.1.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'http_toolkit*'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
